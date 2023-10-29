12caeb6edfdf2075bc2c2b834791c52d01454a2e
Isa-josep <isaurini1902@gmail.com>
2023-10-28 23:44:01 -0600

    Table add

--- PHP/Registro.php
+++ PHP/Registro.php
@@ -1,17 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="../stiles.css">
+</head>
+<body>
 <?php
 include("Conexion.php");
+
 if (isset($_POST["Registrar"])) {
     $a = $_POST['no_control'];
     $b = $_POST['nombre'];
     $c = $_POST['apellido_paterno'];
     $d = $_POST['apellido_materno'];
     $e = $_POST['genero'];
-    date_default_timezone_set('America/Mexico_City');
+    date_default_timezone_set('America/Chihuahua');
     $fecha_entrada = date("Y-m-d H:i:s");
     $regi = "INSERT INTO Alumnos (NO_CONTROL, Nombre, Apellido_Paterno, Apellido_Materno, GENERO, FECHA_ENTRADA)
              VALUES ('$a', '$b', '$c', '$d', '$e', '$fecha_entrada')";
     $query = mysqli_query($conexion, $regi);
     if ($query) {
+        header("Location: http://localhost/Gestor%20de%20biblioteca/");
+        exit();
         echo "Usuario registrado exitosamente.";
     } else {
         echo "Error al registrar el usuario.";
@@ -21,17 +30,32 @@ if (isset($_POST["Registrar"])) {
     $result = mysqli_query($conexion, $consulta);
     if ($result) {
         echo "<h2>Usuarios Registrados:</h2>";
+        echo "<table>
+            <tr>
+                <th class='space'>No. de Control:</th>
+                <th class='space'>Nombre:</th>
+                <th class='space'>Apellido Paterno:</th>
+                <th class='space'>Apellido Materno:</th>
+                <th class='space'>Género:</th>
+                <th class='space'>Fecha de Entrada:</th>
+            </tr>";
         while ($row = mysqli_fetch_assoc($result)) {
-            echo "No. de Control: " . $row['NO_CONTROL'] . "<br>";
-            echo "Nombre: " . $row['Nombre'] . "<br>";
-            echo "Apellido Paterno: " . $row['Apellido_Paterno'] . "<br>";
-            echo "Apellido Materno: " . $row['Apellido_Materno'] . "<br>";
-            echo "Género: " . $row['GENERO'] . "<br>";
-            echo "Fecha de Entrada: " . $row['FECHA_ENTRADA'] . "<br>";
-            echo "<hr>";
+            echo "<tr>
+                <td class='space'>{$row['NO_CONTROL']}</td>
+                <td class='space'>{$row['Nombre']}</td>
+                <td class='space'>{$row['Apellido_Paterno']}</td>
+                <td class='space'>{$row['Apellido_Materno']}</td>
+                <td class='space'>{$row['GENERO']}</td>
+                <td class='space'>{$row['FECHA_ENTRADA']}</td>
+            </tr>";
         }
-    } else {
+        echo "</table>";
+}
+ else {
         echo "Error al consultar la base de datos.";
     }
 }
 ?>
+
+</body>
+</html>
